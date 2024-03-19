ed45e5389cfe830eb1c8f28f70c49a3eab1ea813
ajeetkumarukande95 <163981460+ajeetkumarukande95@users.noreply.github.com>
2024-03-20 00:50:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ import gradio as gr
 import pickle
 import numpy as np
 from PIL import Image
+import io
 
 # Load model from .pkl file
 with open('model.pkl', 'rb') as f:
     model_pkl = pickle.load(f)
 
 # Define a function to make predictions on a single image
-def predict_single_image(image):
+def predict_single_image(file):
+    # Load the image
+    img = Image.open(io.BytesIO(file.read()))
     # Resize and preprocess the image
-    img = image.resize((128, 128))
+    img = img.resize((128, 128))
     img = np.array(img) / 255.0  # Normalization
     img = np.expand_dims(img, axis=0)  # Add batch dimension
 
@@ -28,11 +31,11 @@ def predict_single_image(image):
         return "Without Mask"
 
 # Create a Gradio interface
-image = gr.inputs.Image(shape=(128, 128), label="Upload a photo of a person")
-label = gr.outputs.Label(label="Prediction")
+image = gr.UploadButton(label="Upload a photo of a person")
+label = gr.Label(label="Prediction")
 
 # Create Gradio interface with predict_single_image function
 title = "Face Mask Detection"
 description = "This app detects whether a person in the uploaded image is wearing a face mask or not. Created by Ajeetkumar Ukande."
 author = "Ajeetkumar Ukande"
-gr.Interface(predict_single_image, inputs=image, outputs=label, title=title, description=description, author=author, share=True, debug=True).launch()
+gr.Interface(predict_single_image, inputs=image, outputs=label, title=title, description=description).launch(share=True)
